58f39d5005d21b50ee629496f66be13fb0bd5ad1
lexkirby <107123876+lexkirby@users.noreply.github.com>
2022-06-08 15:17:21 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args){
+        suma(11,15,21);
+
+    }
+    public static void suma(int a, int b, int c){
+        int resultado = a + b + c;
+        System.out.println(resultado);
+
+
+    }
+}
